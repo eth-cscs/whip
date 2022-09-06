dcd906e4be5c3820cb16bfee7fff01381cae3f78
Mikael Simberg <mikael.simberg@iki.fi>
2022-09-06 16:18:17 +0200

    Add license and copyright header

--- cmake/whip-config.cmake
+++ cmake/whip-config.cmake
@@ -1,4 +1,10 @@
-# TODO: license and copyright
+# whip
+#
+# Copyright (c) 2018-2022, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
 
 cmake_policy(VERSION 3.18) # TODO: version...
 
--- cmake/whip_helpers.cmake
+++ cmake/whip_helpers.cmake
@@ -1,4 +1,11 @@
-# TODO: license and copyright
+# whip
+#
+# Copyright (c) 2018-2022, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
 include(CMakeParseArguments)
 
 if("${WHIP_TYPE}" STREQUAL "CUDA")
